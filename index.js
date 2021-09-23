40c3ebf6f626098cfb95ab40a8029a0b1184c05a
Aryan Sethi <aryan.rdps@gmail.com>
2021-09-23 11:04:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid = process.env.TWILIO_SERVICE_SID;
+const client = require('twilio')(accountSid, authToken);
+const cors = require('cors');
+
+
+const port = 3001;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// login -> send sms
+app.get("/login" , cors() , (req , res , next) => {
+
+    // set phonenumber in to
+    client.verify.services(serviceSid)
+             .verifications
+             .create({
+                 to: `+91${req.query.phone}`, 
+                 channel: 'sms'
+                })
+             .then(data => res.status(200).send(data));
+});
+
+app.get("/verify" , cors() , (req , res , next) => {
+
+    
+    client.verify.services(serviceSid)
+        .verificationChecks
+        .create({
+            to: `+91${req.query.phone}`, 
+            code: `${req.query.code}`
+        })
+        .then(data => res.status(200).send(data));
+});
+
+
+app.listen(port , ()=>{
+    console.log(`App running at ${port}`);
+});
